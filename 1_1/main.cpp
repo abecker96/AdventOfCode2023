47cd417b4fc654f48fbf2bd5d329fccd97d806df
Aidan C Becker <acbecker@belcan.com>
2023-12-02 09:49:11 -0500

    Fix main todo issues from last night. No functional changes

--- 1_1/main.cpp
+++ 1_1/main.cpp
@@ -9,16 +9,31 @@
 
 // Day One of Advent of Code.
 // There's a glaring algorithmic improvement I'd like to make, which is to search for the last number from the back of each line.
-// Also I'm pretty sure this method of accessing files line-by-line is terrible...
-// And I haven't commented functions with appropriate headers.
-// Clean up comments tomorrow, probably never worry about the algorithm or file access stuff.
+// Also I'm pretty sure this method of accessing files line-by-line is terrible for performance...
 // Also need to figure out why I can't use iostream..."
 
+
+/**
+* Takes a ASCII character input, and returns numerical value
+*  
+*  @param A numerical character
+*  @return The numerical value of the parameter
+*  @note There is no handling for non-numeric characters, and it only takes ASCII
+*/
 uint8_t lazy_ctoi(char num)
 {
 	return num - '0';
 }
 
+/**
+* Begins checking a string at a given index for substrings representing numbers
+* For example passing the string "2one" and an index of 1 will return 1
+* Passing the string "2one" and an index of 0 will return -1
+*
+* @param A string to check
+* @param The index to search from
+* @return On success: An int 0-9. On Failure: -1
+*/
 int lazy_substrtoi(std::string str, int index)
 {
 	const int keys[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -52,6 +67,12 @@ int lazy_substrtoi(std::string str, int index)
 	return -1;
 }
 
+/**
+* Takes a string representing a line of text as input, and returns the "Calibration Value" as outlined in Prompt.txt
+* 
+* @param A line of input to check
+* @return The "Calibration Value" for that line. Should never be greater than 99.
+*/
 uint8_t getCalibrationValue(std::string line)
 {
 	bool found_first = false;
